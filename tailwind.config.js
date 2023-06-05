88dadb78351cbcd7965ee2f15e6139cc9c8f4670
rahimwebdeveloper <inf.abdurrahim12@gmail.com>
2023-06-05 18:25:25 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  themes: [
+    {
+      mytheme: {
+        primary: "#570df8",
+
+        secondary: "#f000b8",
+
+        accent: "#1dcdbc",
+
+        neutral: "#2b3440",
+
+        "base-100": "#ffffff",
+
+        info: "#3abff8",
+
+        success: "#36d399",
+
+        warning: "#fbbd23",
+
+        error: "#f87272",
+      },
+    },
+  ],
+  plugins: [require("daisyui")],
+};
