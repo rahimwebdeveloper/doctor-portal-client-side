ae08d759a22ff49f2bddd329a221cce218dbb71d
rahimwebdeveloper <inf.abdurrahim12@gmail.com>
2023-06-06 17:37:33 +0600

    banner adde

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import chair from "../../../assets/images/chair.png";
+import bgImage from "../../../assets/images/bg.png";
+
+const Banner = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
+      <div className="hero-content   fle x-col lg:flex-row-reverse">
+        <img src={chair} className="max-w-lg lg:ml-6 rounded-lg  shadow-2xl" alt="" />
+        <div>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>this is home page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
